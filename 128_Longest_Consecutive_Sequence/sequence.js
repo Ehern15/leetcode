1556dcc8edbd530eef07c7ff74f073251b4142da
Ehern15 <erikhern15@gmail.com>
2024-03-15 17:40:23 -0400

    submission rejected trying a different approach

--- 128_Longest_Consecutive_Sequence/sequence.js
+++ 128_Longest_Consecutive_Sequence/sequence.js
@@ -10,12 +10,15 @@ var longestConsecutive = function(nums) {
     }
     for (let index = 0; index < map1.size ; index++) {
         if(map1.get(index - 1) === (map1.get(index) - 1) || map1.get(index + 1) === (map1.get(index) + 1))
-            map2.set(map1.get(index));      
+            map2.set(map1.get(index));
+            if (map1.size == 1){
+                return map2.size;
+            }      
         
     }
     return map2.size;
 };
 
-let nums1 = [100,4,200,1,3,2];
+let nums1 = [-100,4,200,1,3,2];
 let nums2 = [0,3,7,2,5,8,4,6,0,1];
 console.log(longestConsecutive(nums2));
