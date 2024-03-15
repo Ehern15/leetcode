5dc3ec0acdb95b388249b3210a01b56e2f072cc1
Ehern15 <erikhern15@gmail.com>
2024-03-14 22:48:19 -0400

    started 128. Longest Consecutive sequence problem

---
+++ 128_Longest_Consecutive_Sequence/sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const map1 = new Map();
+    const map2 = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        map1.set(nums[i],nums[i]);
+    }
+    for (let index = 0; index < map1.size ; index++) {
+        if(map1.get(index) + 1 == map1.get(index + 1) || map1.get(index) == map1.get(index))
+            map2.set(map1.get(index));      
+        
+    }
+    console.log(map2)
+    return map2.size;
+};
+
+let nums1 = [100,4,200,1,3,2];
+let nums2 = [0,3,7,2,5,8,4,6,0,1];
+console.log(longestConsecutive(nums2));
