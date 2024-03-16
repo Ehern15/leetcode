fe2f0052b66a197e2c58971d83772d8c9fd15b65
Ehern15 <erikhern15@gmail.com>
2024-03-16 17:35:42 -0400

    submission accepted beats 50%

---
+++ 49_Group_Anagrams/anagrams.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+
+var groupAnagrams = function (strs) {
+    const map1 = new Map();
+    let arrayEmpty = Array(26).fill(0);
+
+    for (let i = 0; i < strs.length; i++) {
+        for (let j = 0; j < strs[i].length; j++) {
+            arrayEmpty[strs[i].charCodeAt(j) - 97]++;
+        }
+
+        hashkey = JSON.stringify(arrayEmpty);
+        arrayEmpty.fill(0);
+        let value = [];
+
+        if (map1.has(hashkey)) {
+            value = map1.get(hashkey);
+            value.push(strs[i])
+            map1.set(hashkey, value);
+        } else {
+            value.push(strs[i]);
+            map1.set(hashkey, value);
+        }
+
+    }
+    let solution = new Array();
+    const buildSolution = (value) => solution.push(value);
+    
+    map1.forEach(buildSolution)
+    
+    return solution;
+
+};
+
+
+let input = ["eat", "tea", "tan", "ate", "nat", "bat"];
+groupAnagrams(input);
+input = [""];
+groupAnagrams(input);
+input = ["a"];
+groupAnagrams(input);
