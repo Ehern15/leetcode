2d36d9802fe8dd7f9c948c6f33fb01c4e0fdfdb3
Ehern15 <erikhern15@gmail.com>
2024-03-18 16:27:55 -0400

    submission accepted beats 60%

--- 128_Longest_Consecutive_Sequence/sequence.js
+++ 128_Longest_Consecutive_Sequence/sequence.js
@@ -2,23 +2,50 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
-    const map1 = new Map();
-    const map2 = new Map();
+
+var longestConsecutive = function (nums) {
+    if (nums.length == 0) { return 0 }
+    const set = new Set(nums);
+    let arr = new Array();
     for (let i = 0; i < nums.length; i++) {
-        map1.set(nums[i],nums[i]);
+        if (set.has(nums[i] + 1) || set.has(nums[i] - 1)) {
+            arr.push(nums[i]);
+        }
     }
-    for (let index = 0; index < map1.size ; index++) {
-        if(map1.get(index - 1) === (map1.get(index) - 1) || map1.get(index + 1) === (map1.get(index) + 1))
-            map2.set(map1.get(index));
-            if (map1.size == 1){
-                return map2.size;
-            }      
-        
+    
+    let max, min;
+    for (let i = 0; i < arr.length; i++) {
+        if (max == undefined || min == undefined) {
+            max = arr[i];
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
     }
-    return map2.size;
+    
+    let temp = 1;
+    let sequence = 1;
+    for (let i = min; i <= max; i++) {
+        console.log(i);
+        if (set.has(i) && set.has(i + 1)) {
+            temp++;
+            
+        } else if (temp > sequence) {
+            sequence = temp;
+            temp = 1;
+            
+        } else {
+            temp = 1;
+        }
+    }
+    return sequence;
+
 };
 
-let nums1 = [-100,4,200,1,3,2];
-let nums2 = [0,3,7,2,5,8,4,6,0,1];
-console.log(longestConsecutive(nums2));
+let nums1 = [-100, 4, 200, 1, 3, 2];
+let nums2 = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
+console.log(longestConsecutive(nums1));
