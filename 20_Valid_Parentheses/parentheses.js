7169ee9b5e2d364bef29e79b832f165b56fe6b7b
Ehern15 <erikhern15@gmail.com>
2024-03-19 12:18:16 -0400

    submission accepted but slow working to improve

--- 20_Valid_Parentheses/parentheses.js
+++ 20_Valid_Parentheses/parentheses.js
@@ -2,26 +2,62 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    let stack = [];
-    for(let i = 0; i < s.length;i++){
-        console.log(s.charAt(i))
-        if(s.charAt(i) == '(' || s.charAt(i) == '['|| s.charAt(i) == '{' ){
-            stack.push(s.charAt(i));
-            console.log("push")
-        }
-        if(s.charAt(i) == ')' || s.charAt(i) == ']'|| s.charAt(i) == '}' ){
-            stack.pop();
-            console.log("pop")
-        }
+var isOpen = function (c){
+if(c == '('|| c =='['||c == '{'){
+    return true;
+}else{
+    return false;
+}
+}
+var isClose = function(c){
+    if(c == ')'|| c ==']'||c == '}'){
+        return true;
+    }else{
+        return false;
     }
-    console.log(stack)
-    if (stack === undefined){
+}
+var getOpen = function(c){
+if(c == ')'){
+    return '('
+}
+if(c == ']'){
+    return '['
+}
+if(c == '}'){
+    return '{'
+}
+}
+var peek = function(stack){
+    if(stack.length == 0){
+        return -1;
+    }else{
+        return stack[stack.length-1];
+    }
+}
+var isValid = function (s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        console.log(s.charAt(i))
+        let symbol = s.charAt(i)
+        if (isOpen(symbol)){
+            stack.push(symbol);
+            console.log("push");
+        }
+        if(isClose(symbol) && peek(stack) != getOpen(symbol)){
+            return false;
+        }
+        if(isClose(symbol) && peek(stack) == getOpen(symbol)){
+            stack.pop();
+        }
+
+    }
+    if (stack.length == 0){
         return true;
     }else{
         return false;
     }
 };
+
 let s = "()"
 console.log(isValid(s));
 s = "()[]{}"
