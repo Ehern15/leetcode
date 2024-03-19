d610d9f25ae0847e1656c46de5987871097b29a8
Ehern15 <erikhern15@gmail.com>
2024-03-19 15:29:15 -0400

    submission accepted beats 60%

---
+++ 125_Valid_Palindrome/palindrome.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let string = new String(s);
+    string = string.toLowerCase();
+    string = string.replace(/[^a-z0-9]/g, '');
+
+    for (let i = 0, j = string.length - 1; i < string.length; i++, j--) {
+        if (!(string.charAt(i) == string.charAt(j))) {
+            return false;
+        }
+    }
+    return true;
+
+};
+let s = "A man, a plan, a canal: Panama"
+console.log(isPalindrome(s))
+s = "race a car"
+console.log(isPalindrome(s))
+s = " "
+console.log(isPalindrome(s))
