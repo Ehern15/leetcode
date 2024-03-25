aad6a16bb86887f0b6060937da601378e048bb12
Ehern15 <erikhern15@gmail.com>
2024-03-25 16:59:25 -0400

    started 853 Car Fleet

--- 22_Generate_Parentheses/parentheses.js
+++ 22_Generate_Parentheses/parentheses.js
@@ -3,12 +3,7 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    let openStack = new Array(n);
-    let closeStack = new Array(n);
-    openStack.fill('(');
-    closeStack.fill(')');
-    let str = openStack.concat(closeStack);
-    str = str.join('')
+    
     
 };
 let n = 3
---
+++ 853_Car_Fleet/fleet.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function(target, position, speed) {
+    
+};
+
+let target = 10, position = [3], speed = [3];
+carFleet(target,position,speed);
+target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3];
+carFleet(target,position,speed);
+target = 100, position = [0,2,4], speed = [4,2,1]
+carFleet(target,position,speed);
