84bc6484a627c20f2242567d23ba73a187c2a5ef
Ehern15 <erikhern15@gmail.com>
2024-03-26 14:29:55 -0400

    got stuck on this one studied a solution in pseudocode then implement

--- 22_Generate_Parentheses/parentheses.js
+++ 22_Generate_Parentheses/parentheses.js
@@ -3,8 +3,22 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    
-    
+    const result = [];
+    const recurse = function(l,r,s){
+        if(s.length === 2 * n){
+            result.push(s);
+            return;
+        }
+        if(l < n){
+            recurse(l + 1,r,s + '(');
+        }
+        if(r < l){
+            recurse(l,r + 1,s + ')');
+        }
+        
+    }
+    recurse(0,0,"");
+    return result;
 };
 let n = 3
 generateParenthesis(n);
