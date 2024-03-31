9216832fea6c7492f70d586b6038ac623fb86a5b
Ehern15 <erikhern15@gmail.com>
2024-03-31 16:13:54 -0400

    updated two sum

--- 1_Two_Sum/sum.js
+++ 1_Two_Sum/sum.js
@@ -4,14 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let map = new Map();
+    
     for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(target == nums[i] + nums[j] && i != j){
-                const newArr = new Array(i,j);
-                return newArr;
-            }
+        if(map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
         }
     }
+    return [];
 };
 let nums = [2,7,11,15], target = 9;
 twoSum(nums,target);
