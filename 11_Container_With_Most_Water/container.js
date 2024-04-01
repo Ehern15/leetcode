d464f2a473dc1dbd500c980155879384527e1534
Ehern15 <erikhern15@gmail.com>
2024-04-01 14:27:31 -0400

    completed 11 Container with most water

--- 11_Container_With_Most_Water/container.js
+++ 11_Container_With_Most_Water/container.js
@@ -2,14 +2,24 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
+var maxArea = function (height) {
     let max = 0;
-    for(let i = 0, j = height.length - 1;i < height - 1;i++,j--){
-        max = Math.max(max,(j-i)*Math.min(height[i],height[j]))
+    let i = 0;
+    let j = height.length - 1;
+    while (i < j) {
+        let length = Math.min(height[i], height[j]);
+        let width = j - i;
+        max = Math.max(max, length * width);
+        if (height[i] <= height[j]) {
+            i++;
+        } else {
+            j--;
+        }
     }
+
     return max;
 };
-let height = [1,8,6,2,5,4,8,3,7];
+let height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 console.log(maxArea(height));
-height = [1,1]
+height = [1, 1]
 console.log(maxArea(height));
