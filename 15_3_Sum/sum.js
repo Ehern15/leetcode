e143a1ff602122b4c17363551026858a4f35d7fd
Ehern15 <erikhern15@gmail.com>
2024-04-05 12:14:38 -0400

    3sum

--- 15_3_Sum/sum.js
+++ 15_3_Sum/sum.js
@@ -3,16 +3,30 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
+    if(nums.length == 3){
+        return nums;
+    }
     let map = new Map();
-    let answer = [];
-    
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i])){
+            let temp = map.get(nums[i]);
+            temp.push(i);
+        }else{
+            map.set(nums[i],[i])
         }
     }
+    for(let i = 0;i < nums.length;i++){
+        if(map.get(nums[i]) == []){continue;}
+        for(let j = 0;j < nums.length;j++){
+            if(map.get(nums[j])){
+                
+            }
+        }
+    }
+    let answer = [];
     
-        return answer;
+    
+    return answer;
 }
     let nums = [-1, 0, 1, 2, -1, -4];
     console.log(threeSum(nums));
