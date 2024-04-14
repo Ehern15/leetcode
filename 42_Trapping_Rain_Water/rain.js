e75643a7fd50b9bb18ef4ff48fa3751278e0a531
Erik Hernandez <91921621+Ehern15@users.noreply.github.com>
2024-04-14 15:17:51 -0400

    submisison accepted for 42 trapping rain water

--- 42_Trapping_Rain_Water/rain.js
+++ 42_Trapping_Rain_Water/rain.js
@@ -3,32 +3,48 @@
  * @return {number}
  */
 var trap = function(height) {
-    let i = 0;
-    let j = i + 1;
-    let k = height.length - 1;
     let water = 0;
-    while(i < height.length){
-        //console.log(i,j);
-        let count = 0;
-        while(height[j] < height[i] && j < height.length){
-            //console.log("j:",height[j]);
-            count += (height[i] - height[j]);
-            //console.log("count: ",count);
-            j++;
+    let maxLeft = [];
+    let maxRight = [];
+    let maxWater = new Array(height.length);
+    let max = 0;
+    for(let i = 0; i <height.length;i++){
+        if(i == 0){
+            maxLeft[i] = 0;
+            max = height[i];
+            continue;
         }
-        //console.log("i:",height[i]);
-        if(height[j]>=height[i]){
-            water+=count;
-            i = j;
-            j = i + 1;
-        }else{
-            i++;
-            j = i + 1;
+
+        maxLeft[i] = max;
+
+        if(height[i] > max){
+            max = height[i];
         }
-        
-        //console.log("water: ", water);
+            
         
     }
+    max = 0;
+    for(let i = height.length - 1; i >= 0;i--){
+        if(i == height.length - 1){
+            maxRight[i] = 0;
+            max = height[i];
+            continue;
+        }
+
+        maxRight[i] = max;
+
+        if(height[i] > max){
+            max = height[i];
+        }
+    }
+    for(let i = 0; i < height.length;i++){
+        maxWater[i] = Math.min(maxRight[i],maxLeft[i]) - height[i];
+        if(maxWater[i] > 0){
+            water += maxWater[i];
+        }
+    }
+    
+    
     return water;
 };
 
@@ -36,3 +52,6 @@ let height = [0,1,0,2,1,0,1,3,2,1,2,1];
 console.log("answer: " ,trap(height));
 height = [4,2,0,3,2,5];
 console.log("answer: " ,trap(height));
+height = [4,2,3];
+console.log("answer: " ,trap(height));
+
